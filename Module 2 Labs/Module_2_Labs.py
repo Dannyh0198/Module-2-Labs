fe101df9215a95970e6552c6332119add2d69c12
Danny Hayward <Dannyh0198@hotmail.co.uk>
2021-09-21 18:15:22 +0100

    New

--- Module 2 Labs/Module_2_Labs.py
+++ Module 2 Labs/Module_2_Labs.py
@@ -113,3 +113,39 @@ print("Seconds in 2 hours: ", a * seconds) # printing the number of seconds in a
 print ("Goodbye") #printing Goodbye
 a = 3 # number of hours
 print("Seconds in 3 hours:", a * seconds) #this is the end of the program that computes the number of seconds in 3 hour
+
+# 2.6.1.9 LAB: Simple input and output
+#Your task is to complete the code in order to evaluate the results of four basic arithmetic operations.
+#The results have to be printed to the console.
+#You may not be able to protect the code from a user who wants to divide by zero. That's okay, don't worry about it for now.
+#Test your code - does it produce the results you expect?
+#We won't show you any test data - that would be too simple.
+var_a = float(input("Variable A:"))# input a float value for variable a here
+var_b = float(input("Variable B:"))# input a float value for variable b here
+
+print (var_a + var_b)# output the result of addition here
+print (var_a - var_b)# output the result of subtraction here
+print (var_a * var_b)# output the result of multiplication here
+print (var_a / var_b)# output the result of division here
+
+print("\nThat's all, folks!")
+
+#2.6.1.10 LAB: Operators and expressions
+# Your task is to complete the code in order to evaluate the following expression:
+# PICTURE OF EQUATION
+# The result should be assigned to y. Be careful - watch the operators and keep their priorities in mind.
+# Don't hesitate to use as many parentheses as you need.
+# You can use additional variables to shorten the expression (but it's not necessary). Test your code carefully.
+x = float(input("Enter value for x: "))
+y = 1/((x+1/(x+1/(x+1/x))))
+print("y =", y)
+
+#2.6.1.11 LAB: Operators and expressions
+#Your task is to prepare a simple code able to evaluate the end time of a period of time, given as a number of minutes (it could be arbitrarily large).
+#The start time is given as a pair of hours (0..23) and minutes (0..59). The result has to be printed to the console.
+#For example, if an event starts at 12:17 and lasts 59 minutes, it will end at 13:16.
+#Don't worry about any imperfections in your code - it's okay if it accepts an invalid time - the most important thing is that the code produce valid results for valid input data.
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+print ((hour+(mins + dura)//60)%24, (mins + dura) % 60, sep=":")
---
+++ Module 2 Labs/Working Area.py
@@ -0,0 +1,2 @@
+n = int(input ("Input N:"))
+print(n >= 100)
